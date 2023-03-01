67edbf6fe9031ce532a39471c3e0994f78f04e86
Ayon Paul <ayon.paul@stonybrook.edu>
2023-02-28 20:09:45 -0500

    Finished part 1 for now (Could still be WIP)

--- paul-ayon-assignment2/analysis_pcap_tcp.py
+++ paul-ayon-assignment2/analysis_pcap_tcp.py
@@ -1,6 +1,5 @@
 import dpkt
 import socket
-from dpkt.compat import compat_ord
 
 sender = '130.245.145.12'
 receiver = '128.208.2.198'
@@ -9,6 +8,7 @@ def get_ip(ip):
     return socket.inet_ntop(socket.AF_INET, ip) #Can assume sender & receiver are IP4 addrs
 
 def analysis_pcap_tcp(file):
+    ############### PART 1 ###############
     f = open(file, 'rb')
     pcap = dpkt.pcap.Reader(f)
 
@@ -19,14 +19,13 @@ def analysis_pcap_tcp(file):
         ip_packet = eth.data
 
         if get_ip(ip_packet.src) != sender:
-            continue #Disregard flows where src port is not sender
+            continue #Disregard packets sent from receiver FOR NOW....
 
         #If the data portion of the ip packet is a TCP segment, parse the TCP segment
         if isinstance(ip_packet.data, dpkt.tcp.TCP):  
             tcp_segment = ip_packet.data
-
             flow_key = f'({tcp_segment.sport}, {get_ip(ip_packet.src)}, {tcp_segment.dport}, {get_ip(ip_packet.dst)})'
-    
+ 
             #TCP flow is identified by the src dst tuple
             #Add this TCP packet to said TCP flow
             if flow_key not in flows:
@@ -35,23 +34,35 @@ def analysis_pcap_tcp(file):
             
             flows[flow_key].append(ip_packet)
     
-    print(f'Number of flows: {len(flows)}')   
+    print(f'Number of flows: {len(flows)}\n')   
 
     for index, flow in enumerate(flows): 
         print(f'Flow #{index+1}: {flow}')
-        flows[flow].sort(key=lambda packet: packet.data.seq) #Make sure the packets are in order by seq #
-        
-        for p_index, packets in enumerate(flows[flow]):
-            tcp_segment = packets.data
+        flows[flow].sort(key=lambda packet: packet.data.seq) #Make packets in order
+
+        total_bytes = 0
+
+        for p_index, packet in enumerate(flows[flow]):
+            tcp_segment = packet.data
 
             sync = tcp_segment.flags & dpkt.tcp.TH_SYN
             ack = tcp_segment.flags & dpkt.tcp.TH_ACK
+        
+            #Skip connection establishment 
+            #2 rather than 3 because synack got disregarded from prev loop
+            if p_index < 2:
+                continue
 
-            if sync or p_index < 2:
-                continue #Ignore sync and synack transactions
-                
-            print(f'flow {index+1} | Transaction {p_index-1}: Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
+            #Only add to throughput if past connection est. and ack (going up to last ack)
+            total_bytes += len(tcp_segment.__bytes__()) if ack else 0
+            
+            if p_index < 4: #Only print the first 2 transactions
+                print(f'flow {index+1} | Transaction {p_index-1}: Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
 
+        print(f'Throughput: {total_bytes} bytes\n')
+    
+    ############### PART 2 ###############
+    ##TODO
     f.close()
 
 analysis_pcap_tcp('assignment2.pcap')
