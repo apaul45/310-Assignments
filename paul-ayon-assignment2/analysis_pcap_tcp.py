c66ad1629b36f7e2fe2b9183b64c483d0161c803
Ayon Paul <ayon.paul@stonybrook.edu>
2023-03-06 02:12:56 -0500

    Summed up RTT

--- paul-ayon-assignment2/analysis_pcap_tcp.py
+++ paul-ayon-assignment2/analysis_pcap_tcp.py
@@ -9,6 +9,9 @@ def get_ip(ip):
     return socket.inet_ntop(socket.AF_INET, ip)  # Can assume sender & receiver are IP4 addrs
 
 def compare_packets(packet1, packet2):
+    packet1 = packet1["packet"]
+    packet2 = packet2["packet"]
+
     return packet1.data.seq == packet2.data.seq and packet1.data.ack == packet2.data.ack
 
 def compare_sender_receiver(sender_transactions, tcp):
@@ -31,17 +34,19 @@ def analysis_pcap_tcp(file):
         # If the data portion of the ip packet is a TCP segment, parse the TCP segment
         if isinstance(ip_packet.data, dpkt.tcp.TCP):
             tcp_segment = ip_packet.data
+
             flow_key = f'({tcp_segment.sport}, {get_ip(ip_packet.src)}, {tcp_segment.dport}, {get_ip(ip_packet.dst)})'
+            flow_packet = dict(packet=ip_packet, timestamp=timestamp)
 
             if get_ip(ip_packet.src) != sender:
                 flow_key = f'({tcp_segment.dport}, {get_ip(ip_packet.dst)}, {tcp_segment.sport}, {get_ip(ip_packet.src)})'
 
             # Add this TCP packet to said TCP flow if it hasn't been already
             if flow_key not in flows:
-                flows[flow_key] = [ip_packet]
+                flows[flow_key] = [flow_packet]
                 continue
 
-            flows[flow_key].append(ip_packet)
+            flows[flow_key].append(flow_packet)
 
     print(f'Number of flows: {len(flows)}\n')
 
@@ -50,16 +55,20 @@ def analysis_pcap_tcp(file):
 
         total_bytes = 0
         transactions = [[], []]
+        rtt = flows[flow][0]["timestamp"]
 
-        for p_index, packet in enumerate(flows[flow]):
-            ip = get_ip(packet.src)
-            tcp_segment = packet.data
+        for p_index, p in enumerate(flows[flow]):
+            ip = get_ip(p["packet"].src)
+            tcp_segment = p["packet"].data
 
             if tcp_segment.flags & dpkt.tcp.TH_FIN:
                 break
 
+            if p_index == 1:
+                rtt = p["timestamp"] - rtt
+
             # Skip connection establishment and duplicates
-            if tcp_segment.flags & dpkt.tcp.TH_SYN or compare_packets(packet, flows[flow][p_index-1]):
+            if tcp_segment.flags & dpkt.tcp.TH_SYN or compare_packets(p, flows[flow][p_index-1]):
                 continue
 
             #Add to throughput if connection is in progress and packet from sender
@@ -75,7 +84,8 @@ def analysis_pcap_tcp(file):
                 transactions[1].append(tcp_segment)
                 print(f'flow {index + 1}: Receiver -> Sender | Transaction {len(transactions[1])} Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
 
-        print(f'Throughput: {total_bytes} bytes\n')
+        print(f'Throughput: {total_bytes} bytes')
+        print(f'Total RTT: {rtt*1000:.2f} ms\n')
 
     ############### PART 2 ###############
     ##TODO
