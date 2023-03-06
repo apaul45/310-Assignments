6cf563fb76d81045932cc225aa936f2b334d4d6a
Ayon Paul <ayon.paul@stonybrook.edu>
2023-03-06 12:08:58 -0500

    Add receiver check to get correct tranaction

--- paul-ayon-assignment2/analysis_pcap_tcp.py
+++ paul-ayon-assignment2/analysis_pcap_tcp.py
@@ -4,7 +4,6 @@ import socket
 sender = '130.245.145.12'
 receiver = '128.208.2.198'
 
-
 def get_ip(ip):
     return socket.inet_ntop(socket.AF_INET, ip)  # Can assume sender & receiver are IP4 addrs
 
@@ -67,20 +66,19 @@ def analysis_pcap_tcp(file):
             if p_index == 1:
                 rtt = p["timestamp"] - rtt
 
-            # Skip connection establishment and duplicates
-            if tcp_segment.flags & dpkt.tcp.TH_SYN or compare_packets(p, flows[flow][p_index-1]):
+            # Skip connection establishment
+            if tcp_segment.flags & dpkt.tcp.TH_SYN:
                 continue
 
             #Add to throughput if connection is in progress and packet from sender
             total_bytes += len(tcp_segment) if ip == sender else 0
 
-            # Print out first 2 transactions (total of 4 prints)
-            # if (get_ip(packet.src) == sender and len(transactions) < 4) or (get_ip(packet.src) == receiver and compare_packets(packet, transactions[len(transactions)-1])):
+            #Only consider transactions where sender has a payload, for a total of 2 transactions
             if len(transactions[0]) < 2 and ip == sender and len(tcp_segment.data) != 0:
                 transactions[0].append(tcp_segment)
                 print(f'flow {index + 1}: Sender -> Receiver | Transaction {len(transactions[0])} Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
 
-            if len(transactions[1]) < 2 and compare_sender_receiver(transactions[0], tcp_segment):
+            if len(transactions[1]) < 2 and ip == receiver and compare_sender_receiver(transactions[0], tcp_segment):
                 transactions[1].append(tcp_segment)
                 print(f'flow {index + 1}: Receiver -> Sender | Transaction {len(transactions[1])} Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
 
