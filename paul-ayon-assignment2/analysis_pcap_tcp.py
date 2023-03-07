6564493630304449027d8910993a6a7d9da6cee4
Ayon Paul <ayon.paul@stonybrook.edu>
2023-03-07 14:36:36 -0500

    WIP: Added part 2

--- paul-ayon-assignment2/analysis_pcap_tcp.py
+++ paul-ayon-assignment2/analysis_pcap_tcp.py
@@ -7,12 +7,7 @@ receiver = '128.208.2.198'
 def get_ip(ip):
     return socket.inet_ntop(socket.AF_INET, ip)  # Can assume sender & receiver are IP4 addrs
 
-def compare_packets(packet1, packet2):
-    packet1 = packet1["packet"]
-    packet2 = packet2["packet"]
-
-    return packet1.data.seq == packet2.data.seq and packet1.data.ack == packet2.data.ack
-
+#Helper function used to find transactions
 def compare_sender_receiver(sender_transactions, tcp):
     for transaction in sender_transactions:
         if transaction.seq < tcp.ack:
@@ -20,7 +15,6 @@ def compare_sender_receiver(sender_transactions, tcp):
     return False
 
 def analysis_pcap_tcp(file):
-    ############### PART 1 ###############
     f = open(file, 'rb')
     pcap = dpkt.pcap.Reader(f)
 
@@ -50,13 +44,19 @@ def analysis_pcap_tcp(file):
     print(f'Number of flows: {len(flows)}\n')
 
     for index, flow in enumerate(flows):
-        print(f'Flow #{index + 1}: {flow}')
+        print(f'Flow #{index + 1}: {flow}\n')
 
         total_bytes = 0
         transactions = [[], []]
         rtt = flows[flow][0]["timestamp"]
-        startOfWindow = 0
-        congestionWindows = []
+        start_of_window = 0
+        congestion_windows = []
+        sender_packets = dict()
+        total_retransmissions = 0
+        ack_trios = []
+        receiver_count = 0 #Used to identify 3 consecutive matching receiver acks
+        triple_duplicate_acks_transmitted = 0
+        timeout_retransmissions = 0
 
         for p_index, p in enumerate(flows[flow]):
             ip = get_ip(p["packet"].src)
@@ -70,40 +70,69 @@ def analysis_pcap_tcp(file):
                 rtt = timestamp - rtt #Using the synack as estimation for 1 RTT
 
                 #Initialize the very first congestion window at the timestamp of the SYNACK, but don't count SYNACK packet
-                startOfWindow = timestamp
-                congestionWindows.append(0)
+                start_of_window = timestamp
+                congestion_windows.append(0)
 
             # Skip connection establishment
             if tcp_segment.flags & dpkt.tcp.TH_SYN:
                 continue
 
-            #Add to throughput if connection is in progress and packet from sender
-            total_bytes += len(tcp_segment) if ip == sender else 0
+            if ip == sender:
+                receiver_count = 0 #Reset count
+                total_bytes += len(tcp_segment) #Add to throughput if packet from sender
 
-            #Only consider transactions where sender has a payload, for a total of 2 transactions
-            if len(transactions[0]) < 2 and ip == sender and len(tcp_segment.data) != 0:
-                transactions[0].append(tcp_segment)
-                print(f'flow {index + 1}: Sender -> Receiver | Transaction {len(transactions[0])} Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
+                #Only consider transactions where sender has a payload, for a total of 2 transactions
+                if len(transactions[0]) < 2 and len(tcp_segment.data) != 0:
+                    transactions[0].append(tcp_segment)
+                    print(f'flow {index + 1}: Sender -> Receiver | Transaction {len(transactions[0])} Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
 
-            if len(transactions[1]) < 2 and ip == receiver and compare_sender_receiver(transactions[0], tcp_segment):
-                transactions[1].append(tcp_segment)
-                print(f'flow {index + 1}: Receiver -> Sender | Transaction {len(transactions[1])} Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
+                #Check for retransmission
+                if tcp_segment.seq in sender_packets:
+                    total_retransmissions+=1
+
+                    #Check for timeout -- ie, if the RTO has been reached
+                    if sender_packets[tcp_segment.seq] + rtt <= timestamp:
+                        timeout_retransmissions+=1
+
+                    #Determine triple duplicate ack by checking if sender packet matches any 3 consecutive acks
+                    for ack_trio in ack_trios:
+                        ack = flows[flow][ack_trio[0]]["packet"].data.ack
+
+                        if tcp_segment.seq == ack:
+                            triple_duplicate_acks_transmitted+=1
+                            break
+
+                sender_packets[tcp_segment.seq] = timestamp
+            
+            if ip == receiver:
+                receiver_count += 1
+
+                #Only consider the first 2 receiver packets that match sender packets
+                if len(transactions[1]) < 2 and compare_sender_receiver(transactions[0], tcp_segment):
+                    transactions[1].append(tcp_segment)
+                    print(f'flow {index + 1}: Receiver -> Sender | Transaction {len(transactions[1])} Sequence #: {tcp_segment.seq} Ack #: {tcp_segment.ack} Received Window Size: {tcp_segment.win}')
+                
+                #Check if 3 consecutive acks have occurred in a row
+                if receiver_count == 3:
+                    ack_trios.append([p_index-2, p_index-1, p_index])
+                    receiver_count = 0 #Reset count to prevent duplicate pairs
 
             #If timestamp falls within this window (ie, 1 RTT from the start of window), add it
-            if len(congestionWindows) < 4 and len(transactions[0]) > 0:
-                if timestamp <= startOfWindow + rtt:
-                    congestionWindows[-1]+=1
+            if len(congestion_windows) < 4 and len(transactions[0]) > 0:
+                if timestamp <= start_of_window + rtt:
+                    congestion_windows[-1]+=1
                     continue
 
-                startOfWindow = timestamp
-                congestionWindows.append(1)
+                start_of_window = timestamp
+                congestion_windows.append(1)
 
-        print(f'Throughput: {total_bytes} bytes')
+        print(f'\nThroughput: {total_bytes} bytes')
         print(f'Total RTT: {rtt*1000:.2f} ms')
-        print(f'Congestion Window Sizes: {congestionWindows[0:3]}\n')
+        print(f'Congestion Window Sizes: {congestion_windows[0:3]}')
+        print(f'Total retransmissions: {total_retransmissions}')
+        print(f'    Retransmissions due to Triple Duplicate Acks: {triple_duplicate_acks_transmitted}') 
+        print(f'    Retransmissions due to timeout: {timeout_retransmissions}\n')
 
-    ############### PART 2 ###############
-    ##TODO
     f.close()
 
 analysis_pcap_tcp('assignment2.pcap')
